5893702822574ef99ce5103af40694b6d9165acc
Emily Gorelik <egorelik@jahnelgroup.com>
2024-05-13 14:08:47 -0400

    added hobbies info (no maps)

--- src/views/Hobbies.vue
+++ src/views/Hobbies.vue
@@ -1,5 +1,44 @@
 <template>
-  <main class="hobbies-page">
-    <p>Hobbies Page</p>
-  </main>
+  <div class="flex flex-col gap-4 p-8 text-accent">
+    <p class="text-3xl">Hobbies</p>
+    <p class="text-xl">Skiing</p>
+    <p>
+      I have been skiing since I was 6 years old, and to this day there is not
+      feeling that matches the wind on your face as you rush down a slope
+      nearing speeds of 60 mph. I have been all over the east coast, visiting
+      mountains such as Killington, Pico, Lake Placid, Whiteface and so many
+      more! In December 2021 I made my first ski trip out west and discovered
+      knee-deep powder for the first time.
+      <br /><br />
+      After tearing my ACL in December 2022, I was briefly out of commission.
+      However, for the 23/24 season was back on snow (with the help of my
+      amazing surgical and PT team) and back to crushing it!
+    </p>
+    <p class="text-xl">3D Printing</p>
+    <p>
+      In highschool I used a 3D printer for the first time and ever since I was
+      hooked! I bought my own Ender 3 Pro in May 2020 and since then I have
+      tinkered non-stop. I have upgraded my printer to have: an all metal
+      hotend, a glass buildplate, a silent motherboard, auto bed levelling, and
+      even compiled my own firmware that the printer runs on!
+      <br /><br />
+      To date my favorite project has been my low-poly geometric chess set. You
+      can take a look at that project and my other work
+      <a
+        class="text-primary underline hover:text-secondary"
+        href="https://www.thingiverse.com/emgore1016/designs"
+        target="_blank"
+        >here</a
+      >.
+    </p>
+    <p class="text-xl">Traveling</p>
+    <p>
+      Ever since I was little I have been travelling with my family to visit
+      different countries and states. So far, I have been to 9 different
+      countries and 22 US states. My longest trip was when I stayed in Prague,
+      Czech Republic for two months as part of WPI's study abroad program. It
+      was a truly amazing experience and I can't wait to see where else life
+      will take me next!
+    </p>
+  </div>
 </template>

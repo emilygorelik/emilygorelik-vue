b2422dca15b015aed2f385ce1890e0653e4dd468
Emily Gorelik <egorelik@jahnelgroup.com>
2024-05-13 11:07:12 -0400

    about pg

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <main class="about-page">
-    <p>About Page</p>
+  <main class="flex flex-col gap-4 p-8 text-lg text-accent">
+    <p class="text-3xl">About Me</p>
+    <img
+      src="../assets/profile-prof.png"
+      class="mx-auto w-1/6 rounded-full border-2 border-accent p-2"
+    />
+    <p>
+      I am graduate of Worcester Polytechnic Institute (WPI) with a BS in
+      Computer Science and a minor in Data Science. I have loved technology for
+      as long as I can remember and am always thinking about how I can create
+      applications that will benefit myself and others. My career goal is to
+      make technology accessible to everyone by designing intuitive webpages
+      that are easy to navigate and simple to master.
+    </p>
+    <p>
+      When I am not sitting behind my laptop, you can find me tinkering and
+      upgrading my 3D printer, tearing up the slopes (weather dependent), or
+      playing boardgames.
+    </p>
+    <p>
+      Check out my
+      <a class="text-primary underline hover:text-secondary" href="/hobbies"
+        >hobbies</a
+      >
+      tab to find out what I love to do!
+    </p>
   </main>
 </template>

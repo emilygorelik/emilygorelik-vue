84f8ec9374336288862a58b8340d82a29793f2fd
Emily Gorelik <egorelik@jahnelgroup.com>
2024-05-13 11:08:04 -0400

    change navbar order

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,8 +8,8 @@ import NavItem from "./NavItem.vue";
       <img src="../assets/logo-gold.png" alt="" class="mx-auto my-12 w-1/2" />
     </a>
     <NavItem href="/about-me" label="About Me" />
-    <NavItem href="/experience" label="Experience" />
     <NavItem href="/software-projects" label="Software Projects" />
+    <NavItem href="/experience" label="Experience" />
     <NavItem href="/hobbies" label="Hobbies" />
   </div>
 </template>

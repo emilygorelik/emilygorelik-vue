e45ee78de4ba1d9eca707863d797930fb4199a33
Emily Gorelik <egorelik@jahnelgroup.com>
2024-05-14 13:03:46 -0400

    added icons to sidebar for external profiles

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,23 +1,47 @@
 <script setup>
 import NavItem from "./NavItem.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { dom } from "@fortawesome/fontawesome-svg-core";
+
+library.add(far, fab, fas);
 </script>
 
 <template>
   <div class="flex h-full w-60 flex-col bg-primary text-primary-content">
     <a href="/">
-      <img src="../assets/logo-gold.png" alt="" class="mx-auto my-12 w-1/2" />
+      <img src="../assets/logo-gold.png" alt="" class="mx-auto mt-12 w-1/2" />
     </a>
-    <div class="mx-auto flex gap-4 border-2">
-      <a href="https://www.linkedin.com/in/emily-gorelik/" target="_blank"
-        ><img src="../assets/linkedin.svg"
-      /></a>
-      <a href="https://github.com/emilygorelik" target="_blank">th</a>
+    <div class="my-8 flex justify-center gap-1">
+      <a
+        href="https://www.linkedin.com/in/emily-gorelik/"
+        target="_blank"
+        title="LinkedIn"
+      >
+        <font-awesome-icon
+          :icon="['fab', 'linkedin']"
+          class="aspect-square h-8 rounded p-1 text-secondary hover:bg-accent"
+        />
+      </a>
+      <a href="https://github.com/emilygorelik" target="_blank" title="Github">
+        <font-awesome-icon
+          :icon="['fab', 'github']"
+          class="aspect-square h-8 rounded-full p-1 text-secondary hover:bg-accent"
+        />
+      </a>
       <a
         href="https://www.thingiverse.com/emgore1016/designs"
-        class="rounded-full p-2 hover:bg-accent"
         target="_blank"
-        ><img src="../assets/thingiverse.svg"
-      /></a>
+        title="Thingiverse"
+      >
+        <font-awesome-icon
+          :icon="['fas', 't']"
+          class="aspect-square h-8 rounded-full p-1 text-secondary hover:bg-accent"
+        />
+      </a>
     </div>
     <NavItem href="/about-me" label="About Me" />
     <NavItem href="/software-projects" label="Software Projects" />
